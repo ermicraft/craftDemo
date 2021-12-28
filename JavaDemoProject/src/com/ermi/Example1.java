7b3eaecc8dbb7a847eba1a896043db20a66e3560
ermicraft <eggermias@gmail.com>
2021-12-27 22:01:40 -0500

    commit from eclips

---
+++ JavaDemoProject/src/com/ermi/Example1.java
@@ -0,0 +1,14 @@
+package com.ermi;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello Ermias");
+    System.out.println("Hello Ermias");
+    System.out.println("Hello Ermias");
+    System.out.println("Hello Ermias");
+    System.out.println("Hello Ermias");
+	}
+
+}
