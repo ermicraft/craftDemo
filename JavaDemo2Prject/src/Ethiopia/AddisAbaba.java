2ec174c1611fce9a70dd0c05da8fec21f75703ec
ermicraft <eggermias@gmail.com>
2021-12-27 23:22:24 -0500

    commited from eclips

---
+++ JavaDemo2Prject/src/Ethiopia/AddisAbaba.java
@@ -0,0 +1,15 @@
+package Ethiopia;
+
+public class AddisAbaba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Addis Ababa");
+		System.out.println("Hello Addis Ababa");
+		System.out.println("Hello Addis Ababa");
+		System.out.println("Hello Addis Ababa");
+		System.out.println("Hello Addis Ababa");
+		System.out.println("Hello Addis Ababa");
+	}
+
+}
