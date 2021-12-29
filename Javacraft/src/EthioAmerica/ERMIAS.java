164e50759fc7e4d7bd6ee0abea080609895659ce
ermicraft <eggermias@gmail.com>
2021-12-28 21:05:39 -0500

    commit from eclips

---
+++ Javacraft/src/EthioAmerica/ERMIAS.java
@@ -0,0 +1,11 @@
+package EthioAmerica;
+
+public class ERMIAS {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ethiopia Ethiopia Ethiopia");
+		System.out.println("Ethiopia Ethiopia Ethiopia");
+		System.out.println("Ethiopia Ethiopia Ethiopia");
+		System.out.println("Ethiopia Ethiopia Ethiopia");
+}
+}
